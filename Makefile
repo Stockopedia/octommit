2a97d7f76a015b82bc88f60331e95f5eec578c81
Tj Broodryk <tjaart@stockopedia.com>
2021-03-26 19:07:31 +0000

    chore: tidy up

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 docker: 
 	docker build -t stockopedia/octommit:$(version) . --build-arg NPM_TOKEN=$(token) --build-arg TAG=$(version)
+trufflehog:
+	truffleHog --regex --entropy=False . --exclude_paths .truffelhog
--- example/connector/src/index.ts
+++ example/connector/src/index.ts
@@ -1,3 +1,13 @@
 import { Octommit } from "octommit";
 
-new Octommit("").update().org("").repository("").sourceBranch("");
+new Octommit("")
+  .update()
+  .org("")
+  .repository("")
+  .sourceBranch("")
+  .outputBranch("")
+  .sourcePath("")
+  .outputPath("")
+  .set("image.tag", "<repo>/<image>:>tag>")
+  .commit("message")
+  .run();
